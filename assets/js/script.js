297708672770db0b8bf40bf946122b50ab7feb5b
José Gabriel Santos Gomes <70996909+naasdd@users.noreply.github.com>
2023-12-04 17:12:44 -0300

    iniciando parte projetos

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let estilo = document.getElementById('stylesheet')
+let titulo = document.getElementById('titulopag')
+
+function projects(){
+    estilo.href = './assets/css/projetos.css'
+    titulo.innerHTML = 'Projetos'
+}
